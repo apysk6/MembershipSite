2c8c87910763ab32af377397e00085b0120d3516
Artur Pyśk <artcik6@gmail.com>
2018-04-20 14:46:09 +0200

    added overloaded Convert method for single product

--- Memberships/Areas/Admin/Extensions/ConversionExtensions.cs
+++ Memberships/Areas/Admin/Extensions/ConversionExtensions.cs
@@ -35,5 +35,31 @@ namespace Memberships.Areas.Admin.Extensions
                        ProductTypes = types
                    };
         }
+
+        public static async Task<ProductModel> Convert(
+           this Product product, ApplicationDbContext db)
+        {
+            var text = await db.ProductLinkTexts.FirstOrDefaultAsync(
+                p => p.Id.Equals(product.ProductLinkTextId));
+
+            var type = await db.ProductTypes.FirstOrDefaultAsync(
+                p => p.Id.Equals(product.ProductTypeId));
+
+            var model = new ProductModel
+            {
+                Id = product.Id,
+                Title = product.Title,
+                Description = product.Description,
+                ImageUrl = product.ImageUrl,
+                ProductLinkTextId = product.ProductLinkTextId,
+                ProductTypeId = product.ProductTypeId,
+                ProductLinkTexts = new List<ProductLinkText>(),
+                ProductTypes = new List<ProductType>()
+            };
+            model.ProductLinkTexts.Add(text);
+            model.ProductTypes.Add(type);
+
+            return model;
+        }
     }
 }
