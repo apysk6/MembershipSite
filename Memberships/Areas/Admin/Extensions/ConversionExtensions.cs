082c082516f4e1f226952385407cdf8c0b64dbd2
Artur Pyśk <artcik6@gmail.com>
2018-04-20 13:05:48 +0200

    added Conversion Method to fetch ProductModel with data

---
+++ Memberships/Areas/Admin/Extensions/ConversionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Memberships.Areas.Admin.Models;
+using Memberships.Entities;
+using Memberships.Models;
+
+namespace Memberships.Areas.Admin.Extensions
+{
+    public static class ConversionExtensions
+    {
+        public static async Task<IEnumerable<ProductModel>> Convert(
+            this IEnumerable<Product> products, ApplicationDbContext db)
+        {
+            var text = await db.ProductLinkTexts.ToListAsync();
+            var types = await db.ProductTypes.ToListAsync();
+
+            return from p in products
+                   select new ProductModel
+                   {
+                       Id = p.Id,
+                       Title = p.Title,
+                       Description = p.Description,
+                       ImageUrl = p.ImageUrl,
+                       ProductLinkTextId = p.ProductLinkTextId,
+                       ProductTypeId = p.ProductTypeId,
+                       ProductLinkTexts = text,
+                       ProductTypes = types
+                   };
+        }
+    }
+}
