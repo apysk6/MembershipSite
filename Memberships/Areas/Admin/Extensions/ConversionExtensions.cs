186766fdacd17c387c6eb5c65162e5e613217ca4
Artur Pyśk <artcik6@gmail.com>
2018-04-20 16:54:43 +0200

    added overloaded Convert method for ProductItem

--- Memberships/Areas/Admin/Extensions/ConversionExtensions.cs
+++ Memberships/Areas/Admin/Extensions/ConversionExtensions.cs
@@ -79,5 +79,19 @@ namespace Memberships.Areas.Admin.Extensions
                                   p => p.Id.Equals(pi.ProductId)).Title
                           }).ToListAsync();
         }
+
+        public static async Task<ProductItemModel> Convert(
+          this ProductItem productItem, ApplicationDbContext db)
+        {
+
+            var model = new ProductItemModel
+            {
+                ItemId = productItem.ItemId,
+                ProductId = productItem.ProductId,
+                Items = await db.Items.ToListAsync(),
+                Products = await db.Products.ToListAsync(),
+            };
+            return model;
+        }
     }
 }
