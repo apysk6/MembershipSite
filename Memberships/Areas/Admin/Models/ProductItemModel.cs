bb817b1820c5e5e6a46f46098bb5fa9482ce06af
Artur Pyśk <artcik6@gmail.com>
2018-04-20 16:02:09 +0200

    added ProductItemModel

---
+++ Memberships/Areas/Admin/Models/ProductItemModel.cs
@@ -0,0 +1,23 @@
+﻿using Memberships.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Areas.Admin.Models
+{
+    public class ProductItemModel
+    {
+        [DisplayName("Product ID")]
+        public int ProductId { get; set; }
+        [DisplayName("Item ID")]
+        public int ItemId { get; set; }
+        [DisplayName("Product Title")]
+        public string ProductTitle { get; set; }
+        [DisplayName("Item Title")]
+        public string ItemTitle { get; set; }
+        public ICollection<Product> Products { get; set; }
+        public ICollection<Item> Items { get; set; }
+    }
+}
