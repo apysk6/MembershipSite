1237126bb27478d7936be5cefe3917fa3ba5f8e1
Artur Pyśk <artcik6@gmail.com>
2018-04-23 18:51:25 +0200

    added GetUsers method

--- Memberships/Extensions/IdentityExtensions.cs
+++ Memberships/Extensions/IdentityExtensions.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Principal;
 using System.Web;
+using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace Memberships.Extensions
 {
@@ -16,5 +18,17 @@ namespace Memberships.Extensions
 
             return user != null ? user.FirstName : String.Empty;
         }
-    }
+
+        public static async Task GetUsers(this List<UserViewModel> users)
+        {
+            var db = ApplicationDbContext.Create();
+            users.AddRange(await (from u in db.Users
+                                  select new UserViewModel
+                                  {
+                                      Id = u.Id,
+                                      Email = u.Email,
+                                      FirstName = u.FirstName
+                                  }).OrderBy(o => o.Email).ToListAsync());
+        }
+    
 }
--- Memberships/Models/UserViewModel.cs
+++ Memberships/Models/UserViewModel.cs
@@ -9,7 +9,7 @@ namespace Memberships.Models
     public class UserViewModel
     {
         [Display(Name = "User ID")]
-        public int Id { get; set; }
+        public string Id { get; set; }
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
