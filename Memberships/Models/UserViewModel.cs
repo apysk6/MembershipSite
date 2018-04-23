41c71ba50f49d4a5c530c7f4dbb997bc044b121b
Artur Pyśk <artcik6@gmail.com>
2018-04-23 17:42:50 +0200

    added UserViewModel

---
+++ Memberships/Models/UserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Models
+{
+    public class UserViewModel
+    {
+        [Display(Name = "User ID")]
+        public int Id { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [StringLength(30, ErrorMessage = "The {0} must be at least {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(100)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
