f5e947329d4c5fbe73b833fe6e135ba4ea074d18
Artur Pyśk <artcik6@gmail.com>
2018-05-03 01:42:55 +0200

    altered GetProductSectionsAsync method in SectionExtensions.cs

--- Memberships/Extensions/SectionExtensions.cs
+++ Memberships/Extensions/SectionExtensions.cs
@@ -21,13 +21,16 @@ namespace Memberships.Extensions
                 join i in db.Items on pi.ItemId equals i.Id
                 join s in db.Sections on i.SectionId equals s.Id
                 where p.Id.Equals(productId)
-                orderby s.Title
+                orderby s.Id
                 select new ProductSection
                 {
                     Id = s.Id,
                     ItemTypeId = i.ItemTypeId,
                     Title = s.Title,
                 }).ToListAsync();
+
+            foreach (var section in sections)
+                section.Items = await GetProductItemRowsAsync(productId, section.Id, section.ItemTypeId, userId);
 
             var result = sections.Distinct(new ProductSectionEqualityComparer()).ToList();
             var model = new ProductSectionModel
