f3780d80078785500841d8e1fddf8be4aca1cd3c
Artur Pyśk <artcik6@gmail.com>
2018-05-02 18:22:13 +0200

    added ProductContentController

---
+++ Memberships/Controllers/ProductContentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Memberships.Controllers
+{
+    [Authorize]
+    public class ProductContentController : Controller
+    {
+        // GET: ProductContent
+        public async Task<ActionResult> Index(int id)
+        {
+            return View();
+        }
+    }
+}
