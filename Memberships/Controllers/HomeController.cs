e64b74103942b4f89b5e475d0b37847f86955426
Artur Pyśk <artcik6@gmail.com>
2018-05-02 13:22:51 +0200

    added ThumbnailExtensions.cs

--- Memberships/Controllers/HomeController.cs
+++ Memberships/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
 namespace Memberships.Controllers
 {
@@ -11,6 +12,7 @@ namespace Memberships.Controllers
     {
         public ActionResult Index()
         {
+            var userId = Request.IsAuthenticated ? HttpContext.User.Identity.GetUserId() : null;
             var model = new List<ThumbnailAreaModel>();
             model.Add(new ThumbnailAreaModel
             {
---
+++ Memberships/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.Owin;
+using System.Security.Claims;
+
+namespace Memberships.Extensions
+{
+    public static class HttpContextExtensions
+    {
+        private const string nameidentifier =
+            "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier";
+
+        public static string GetUserId(this HttpContextBase ctx)
+        {
+            var uid = string.Empty;
+
+            try
+            {
+                var claims = ctx.GetOwinContext().Get<ApplicationSignInManager>()
+                    .AuthenticationManager.User.Claims.FirstOrDefault(claim =>
+                    claim.Type.Equals(nameidentifier));
+
+                if (claims != default(Claim))
+                    uid = claims.Value;
+            }
+            catch
+            {
+
+            }
+
+            return uid;
+        }
+    }
+}
