05582ae30ab55460d6a0aa7847e34b3887a332c4
Artur Pyśk <artcik6@gmail.com>
2018-05-02 18:46:38 +0200

    added ProductSectionEqualityComparer

---
+++ Memberships/Comparers/ProductSectionEqualityComparer.cs
@@ -0,0 +1,21 @@
+﻿using Memberships.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Comparers
+{
+    public class ProductSectionEqualityComparer : IEqualityComparer<ProductSection>
+    {
+        public bool Equals(ProductSection section1, ProductSection section2)
+        {
+            return section1.Id.Equals(section2.Id);
+        }
+
+        public int GetHashCode(ProductSection section)
+        {
+            return (section.Id).GetHashCode();
+        }
+    }
+}
