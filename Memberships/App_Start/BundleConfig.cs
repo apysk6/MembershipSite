0f3683ff8411843a6e7335e377220c2bbc636694
Artur Pyśk <artcik6@gmail.com>
2018-05-04 01:25:11 +0200

    added RegisterCode.js scripts

--- Memberships/App_Start/BundleConfig.cs
+++ Memberships/App_Start/BundleConfig.cs
@@ -32,7 +32,8 @@ namespace Memberships
 
             bundles.Add(new ScriptBundle("~/bundles/ui").Include(
                       "~/Scripts/carret.js",
-                      "~/Scripts/JWPlayer.js"));
+                      "~/Scripts/JWPlayer.js",
+                      "~/Scripts/RegisterCode.js"));
 
             bundles.Add(new StyleBundle("~/Content/membership").Include(
                     "~/Content/navbar.css",
