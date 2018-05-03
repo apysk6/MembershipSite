4535735cd3c5a5d9867bccab9fa49a93dea627cb
Artur Pyśk <artcik6@gmail.com>
2018-05-03 13:45:11 +0200

    altered GetProductItemRowsAsync method in SectionExtensions

--- Memberships/Extensions/SectionExtensions.cs
+++ Memberships/Extensions/SectionExtensions.cs
@@ -21,7 +21,7 @@ namespace Memberships.Extensions
                 join i in db.Items on pi.ItemId equals i.Id
                 join s in db.Sections on i.SectionId equals s.Id
                 where p.Id.Equals(productId)
-                orderby s.Id
+                orderby s.Title
                 select new ProductSection
                 {
                     Id = s.Id,
@@ -33,9 +33,13 @@ namespace Memberships.Extensions
                 section.Items = await GetProductItemRowsAsync(productId, section.Id, section.ItemTypeId, userId);
 
             var result = sections.Distinct(new ProductSectionEqualityComparer()).ToList();
+
+            var union = result.Where(r => !r.Title.ToLower().Contains("download"))
+                .Union(result.Where(r => r.Title.ToLower().Contains("download")));
+
             var model = new ProductSectionModel
             {
-                Sections = result,
+                Sections = union.ToList(),
                 Title = await (from p in db.Products
                                where p.Id.Equals(productId)
                                select p.Title).FirstOrDefaultAsync()
