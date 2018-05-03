1e19785153442cc82b043173b288df1ee0bb9308
Artur Pyśk <artcik6@gmail.com>
2018-05-04 01:02:56 +0200

    added RegisterCodeController and Register action

---
+++ Memberships/Controllers/RegisterCodeController.cs
@@ -0,0 +1,29 @@
+﻿using Memberships.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Memberships.Controllers
+{
+    public class RegisterCodeController : Controller
+    {
+        public async Task<ActionResult> Register(string code)
+        {
+            if(Request.IsAuthenticated)
+            {
+                var userId = HttpContext.GetUserId();
+                var registered = await SubscriptionExtension
+                    .RegisterUserSubscriptionCode(userId, code);
+
+                if (!registered) throw new ApplicationException();
+
+                return PartialView("_RegisterCodePartial");
+            }
+
+            return View();
+        }
+    }
+}
