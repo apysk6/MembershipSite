baa32914a16673c0b2f47514170871136912d958
Artur Pyśk <artcik6@gmail.com>
2018-05-03 18:30:39 +0200

    added ContentViewModel

---
+++ Memberships/Models/ContentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Models
+{
+    public class ContentViewModel
+    {
+        public int ProductId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string HTML { get; set; }
+        public string VideoURL { get; set; }
+    }
+}
