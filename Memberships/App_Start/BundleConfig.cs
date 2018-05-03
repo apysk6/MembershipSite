3ebb4627981350eb2570f87f162ae8080d8929c1
Artur Pyśk <artcik6@gmail.com>
2018-05-03 19:58:25 +0200

    added RegisterCode.css

--- Memberships/App_Start/BundleConfig.cs
+++ Memberships/App_Start/BundleConfig.cs
@@ -38,7 +38,8 @@ namespace Memberships
                     "~/Content/navbar.css",
                     "~/Content/thumbnails.css",
                     "~/Content/ProductContent.css",
-                    "~/Content/carret.css"));
+                    "~/Content/carret.css",
+                    "~/Content/RegisterCode.css"));
 
 
         }
