10502fde9e29ae5b7b1e968f693156d4331c102b
Artur Pyśk <artcik6@gmail.com>
2018-05-03 17:40:34 +0200

    added ProductContent Route to RouteConfig.cs

--- Memberships/App_Start/BundleConfig.cs
+++ Memberships/App_Start/BundleConfig.cs
@@ -31,7 +31,8 @@ namespace Memberships
                       "~/Scripts/AdminMenu.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/ui").Include(
-                      "~/Scripts/carret.js"));
+                      "~/Scripts/carret.js",
+                      "~/Scripts/JWPlayer.js"));
 
             bundles.Add(new StyleBundle("~/Content/membership").Include(
                     "~/Content/navbar.css",
