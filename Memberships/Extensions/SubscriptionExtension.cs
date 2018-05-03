3e03c69bfb97b37bbdda83f371da830bbaf3b8f1
Artur Pyśk <artcik6@gmail.com>
2018-05-03 20:23:46 +0200

    added SubscriptionExtensions class and GetSubscriptionIdByRegistrationCode method

---
+++ Memberships/Extensions/SubscriptionExtension.cs
@@ -0,0 +1,35 @@
+﻿using Memberships.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Memberships.Extensions
+{
+    public static class SubscriptionExtension
+    {
+        public static async Task<int> GetSubscriptionIdByRegistrationCode(
+            this IDbSet<Subscription> subscription, string code)
+        {
+            try
+            {
+                if (subscription == null || code.Equals(String.Empty))
+                    return Int32.MinValue;
+
+                var subscriptionId = await (
+                    from s in subscription
+                    where s.RegistrationCode.Equals(code)
+                    select s.Id).FirstOrDefaultAsync();
+
+                return subscriptionId;
+            }
+
+            catch
+            {
+                return Int32.MinValue;
+            }
+        }
+    }
+}
